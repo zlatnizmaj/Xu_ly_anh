83e96507d4d3536ed3a2c8691f447c77b77de42c
MrGao <decakgao@gmail.com>
2018-07-02 16:13:15 +0700

    modified 020718

--- Moj Python/Numpy/Mang 1 chieu.py
+++ Moj Python/Numpy/Mang 1 chieu.py
@@ -31,9 +31,39 @@ print(y)
 one_array_like = np.ones(x)
 print(one_array_like)
 
+# Array of zeros with the same shape and type as a.
+# Return array of given shape and type as given array, with zeros,
+# numpy.zeros_like method
+array = np.arange(10).reshape(5, 2)
+print("Original array : \n", array)
+
+b = np.zeros_like(array, float)
+print("\nMatrix b : \n", b)
+
+array = np.arange(8)
+c = np.zeros_like(array)
+print("\nMatrix c: \n", c)
+
+array = np.arange(4).reshape(2, 2)
+c = np.zeros_like(array, dtype = 'float')
+print("\nMatrix  : \n", c)
+
+
 #  tạo mảng các số nguyên từ 0 đến n-1 (n số tổng cộng)
 m = np.arange(0, 10, 2) #arrange(start, stop, step)
-print(m)
+print('\n', m)
 print(np.arange(3))
 print(np.arange(2,5))
 print(np.arange(5, 1, -0.9))
+
+a = np.arange(0, 11)
+b = 2
+x = b**a #
+print(x)
+
+c = [3]*10
+# for i in range(1,11):
+#     c.append(3)
+c[9] = 1.5
+y = np.array(c)
+print(y)
