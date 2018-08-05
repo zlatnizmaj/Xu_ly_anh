e5a1308d21e9ad249f1571bf0164005e3f3b2f1e
MrGao <decakgao@gmail.com>
2018-08-06 06:43:48 +0700

    modified 060818

---
+++ Moj Python/PCA/pca2.py
@@ -0,0 +1,16 @@
+from sklearn import datasets
+import sklearn.datasets
+from scikit import pearson_correlation
+
+
+from scikits.crab.models import MatrixPreferenceDataModel
+from scikits.crab.metrics import pearson_correlation
+from scikits.crab.similarities import UserSimilarity
+from scikits.crab.recommenders.knn import UserBasedRecommender
+
+movies = datasets.load_sample_movies() # Load the dataset
+model = MatrixPreferenceDataModel(movies.data) # Build the model
+similarity = UserSimilarity(model, pearson_correlation) # Build the similarity
+recommender = UserBasedRecommender(model, similarity, with_preference=True) # Build the User based recommender
+
+recommender.recommend(7)  # Recommend items for the user 7
