30a2feef0cce3549a568cd3000b531f4acf0f924
MrGao <decakgao@gmail.com>
2018-08-07 22:40:39 +0700

    commit

---
+++ Moj Python/PCA/PCA01.py
@@ -0,0 +1,12 @@
+# http://sebastianraschka.com/Articles/2015_pca_in_3_steps.html#about-iris
+import pandas as pd
+
+df = pd.read_csv(
+    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+    header=None,
+    sep=',')
+
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
+df.dropna(how="all", inplace=True) # drops the empty line at file-end
+
+print(df.tail())
