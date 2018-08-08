5775596d9cba4be6982b8ecfcf5e641b83c0d071
MrGao <decakgao@gmail.com>
2018-08-08 07:04:47 +0700

    commited 080818

--- Moj Python/SVMs/Stock-Forecast-master/Stock-Forecast-master/stock.py
+++ Moj Python/SVMs/Stock-Forecast-master/Stock-Forecast-master/stock.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 dates = []
 prices = []
 
+
 def get_data(filename):
     with open(filename, 'r') as csvfile:
         csvFileReader = csv.reader(csvfile)
@@ -15,26 +16,28 @@ def get_data(filename):
             dates.append(int(row[0].split('-')[0]))
             prices.append(float(row[1]))
         return
-    
+
+
 def predict_prices(dates, prices, x):
     dates = np.reshape(dates, (len(dates)), 1)
-   
+
     svr_lin = SVR(kernel='linear', C=1e3)
-    svr_poly = SVR(kernel = 'poly', C=1e3 , degree = 2 )
-    svr_rbf = SVR(kernel = 'rbf', C=1e3 , gamma = 0.1)
-    svr_lin.fit(dates, prices)    
+    svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    svr_lin.fit(dates, prices)
     svr_poly.fit(dates, prices)
     svr_rbf.fit(dates, prices)
-    
-    plt.scatter(dates, prices, color='black', label = 'data')
-    plt.plot(dates, svr_rbf.predict(dates), color= 'red', label ='RBF model')
-    plt.plot(dates, svr_lin.predict(dates), color= 'green', label ='linear model')
-    plt.plot(dates, svr_poly.predict(dates), color= 'blue', label ='polynomial model')
+
+    plt.scatter(dates, prices, color='black', label='data')
+    plt.plot(dates, svr_rbf.predict(dates), color='red', label='RBF model')
+    plt.plot(dates, svr_lin.predict(dates), color='green', label='linear model')
+    plt.plot(dates, svr_poly.predict(dates), color='blue', label='polynomial model')
     plt.xlabel('Dates')
     plt.ylabel('Price')
     plt.title('Support Vector Regresion')
-    
+
     return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+
 
 get_data('aapl.csv')
 
