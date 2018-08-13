4aa31d5c47f62a81ab0ad8b116026117b66e2bb2
MrGao <decakgao@gmail.com>
2018-08-13 16:05:11 +0700

    commited 130818

--- Moj Python/SVMs/copyNAM-SON SVM/01 Code_py/08 OKAlgorithm.py
+++ Moj Python/SVMs/copyNAM-SON SVM/01 Code_py/08 OKAlgorithm.py
@@ -164,14 +164,24 @@ plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
 
 plt.show()
 
+# tuned_parameters = [{'C': [0.01, 0.1, 1, 10, 100],
+#                      'gamma': [0.5, 1, 2, 3, 4]}]
+# clf = GridSearchCV(SVC(kernel='rbf'), tuned_parameters, cv=10, scoring='accuracy', return_train_score=True)
+# clf.fit(X_train, y_train)
+#
+# print(clf.cv_results_)
+# print(clf.best_params_)
+# print(confusion_matrix(y_validation, clf.best_estimator_.predict(X_validation)))
+# print(clf.best_estimator_.score(X_validation, y_validation))
+
 
 # print(X_validation,Y_validation), lưu ra model, mỗi lần change
 
-filename = path_files_CSV + 'SVM_finalized_model.sav'
-
-SVM_model_pkl = open(filename, 'wb')
-dump(SVM, SVM_model_pkl)
-SVM_model_pkl.close()
+# filename = path_files_CSV + 'SVM_finalized_model.sav'
+#
+# SVM_model_pkl = open(filename, 'wb')
+# dump(SVM, SVM_model_pkl)
+# SVM_model_pkl.close()
 
 # Loading the saved decision tree model pickle
 # SVM_model_pkl = open(filename, 'rb')
